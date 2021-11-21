49621d3385b1d6b024c6db3ce9dea4638ceb3f65
AnjanaRita <ritaanjana1993@gmail.com>
2021-11-21 13:03:49 +0530

    adds zeroshot topic inference

---
+++ zeroshot_topics/utils.py
@@ -0,0 +1,8 @@
+from functools import lru_cache
+from transformers import pipeline
+
+
+@lru_cache
+def load_zeroshot_model(model_name="valhalla/distilbart-mnli-12-6"):
+    classifier = pipeline("zero-shot-classification", model=model_name)
+    return classifier
